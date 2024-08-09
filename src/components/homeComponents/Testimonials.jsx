21a222401e9ffa10344de3976315a972e228792d
AbuBakar <skbakar1999@gmail.com>
2024-08-09 18:16:15 +0500

    parallax and conflicts resolve

--- src/components/homeComponents/Testimonials.jsx
+++ src/components/homeComponents/Testimonials.jsx
@@ -3,7 +3,7 @@ import { TestimonialsCard } from "../resuableComponents";
 
 export const Testimonials = () => {
   return (
-    <div className="w-full flex justify-center ">
+    <div className="w-full flex justify-center py-10">
       <div className="md:border-[15px] border-[10px] border-gray-800 md:w-[75%] w-[95%] md:py-6 py-2 md:px-4 px-0">
         <TestimonialsCard />
       </div>
--- src/themes/Layout.jsx
+++ src/themes/Layout.jsx
@@ -6,7 +6,7 @@ export const Layout = ({ children }) => {
     <div>
       <Nav/>
       <div className="pt-[70px]" style={{height:'calc(100vh-nav)'}}>{ children }</div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
